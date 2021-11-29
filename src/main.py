82dcc184dce94b5629f7daef7ccd0301c6b65a4d
linaksel <linrosaksel@gmail.com>
2021-11-29 02:14:00 +0200

    update requirements, code cleanup

--- src/main.py
+++ src/main.py
@@ -5,31 +5,24 @@ import requests
 from uvicorn.config import LOGGING_CONFIG
 from pydantic import BaseModel
 
-import PySimpleGUI as sg
-
 app = FastAPI()
 
 iplist = {}
 
 class Item(BaseModel):
     ip: str
-
-#fliptest = False
-fliptest = True
-
-is_host = True
-host = '0.0.0.0'
+ 
+host = os.environ['CHATHOST']
+own_ip = os.environ['MYIP']
+is_host = (host == '0.0.0.0')
+localhost = '0.0.0.0'
 port = 8000
-if fliptest:
-    is_host= False
-    host = '0.0.0.1'
-    port = 8001
 
 @app.on_event("startup")
 def connect_host():
     if not is_host:
-        url = f"http://{os.environ['CHATHOST']}:8000/join"
-        payload = {"ip": os.environ['MYIP']}
+        url = f"http://{host}:8000/join"
+        payload = {"ip": own_ip}
         response = requests.post(url, json=payload)
         print(response)
 
@@ -48,7 +41,7 @@ def read_root():
 
 def run():
     LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s [%(name)s] %(levelprefix)s %(message)s"
-    uvicorn.run(app, host=host, port=port)
+    uvicorn.run(app, host=localhost, port=port)
 
 if __name__ == "__main__":
     run()
